6c08280c72a06de8297e62979957860ec3e3b6a8
Christopher Toth <q@q-continuum.net>
2016-05-14 20:28:11 -0600

    Add metadata so that script is displayed in input gestures

--- addon/globalPlugins/jumpToError/addon.py
+++ addon/globalPlugins/jumpToError/addon.py
@@ -9,6 +9,7 @@ import win32api
 import error_finder
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	scriptCategory = _("Jump to Error")
 
 	def get_navigator_line(self):
 		info = api.getReviewPosition().copy()
@@ -22,6 +23,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 			return
 		arguments = "/g %s \"%s\"" % (line, file)
 		win32api.ShellExecute(0, 'open', "notepad.exe", arguments, '', 1)
+	script_goToError.__doc__ = _("""Quickly open your editor to the file and line mentioned in a traceback""")
 
 	__gestures = {
 	"KB:NVDA+g":"goToError",
